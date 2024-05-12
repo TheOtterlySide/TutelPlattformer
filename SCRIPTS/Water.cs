d38cc0aaa583260e85cd4df6c0af004611f06d88
The Otterly Side <47543964+TheOtterlySide@users.noreply.github.com>
2024-05-12 14:29:27 +0200

    add: Water Interaction

---
+++ SCRIPTS/Water.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public partial class Water : Area2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void _on_body_entered(Node2D body)
+	{
+		if (body.IsInGroup("Player") && body is Player player)
+		{
+			player.IsInWater(true);
+		}
+	}
+
+	private void _on_body_exited(Node2D body)
+	{
+		if (body.IsInGroup("Player") && body is Player player)
+		{
+			player.IsInWater(false);
+		}
+	}
+}
