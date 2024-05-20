60f2f352f8afb9beaef61e66634b92af61032103
The Otterly Side <47543964+TheOtterlySide@users.noreply.github.com>
2024-05-20 09:58:09 +0200

    add: Wwise Integration

---
+++ GeneratedSoundBanks/wwise_ids.gd
@@ -0,0 +1,56 @@
+class_name AK
+
+class EVENTS:
+
+	const _dict = {}
+
+class STATES:
+
+	const _dict = {}
+
+class SWITCHES:
+
+	const _dict = {}
+
+class GAME_PARAMETERS:
+
+	const _dict = {}
+
+class TRIGGERS:
+
+	const _dict = {}
+
+class BANKS:
+
+	const INIT = 1355168291
+
+	const _dict = {
+		"Init": INIT
+	}
+
+class BUSSES:
+
+	const MASTER_AUDIO_BUS = 3803692087
+
+	const _dict = {
+		"Master Audio Bus": MASTER_AUDIO_BUS
+	}
+
+class AUX_BUSSES:
+
+	const _dict = {}
+
+class AUDIO_DEVICES:
+
+	const NO_OUTPUT = 2317455096
+	const SYSTEM = 3859886410
+
+	const _dict = {
+		"No_Output": NO_OUTPUT,
+		"System": SYSTEM
+	}
+
+class EXTERNAL_SOURCES:
+
+	const _dict = {}
+
